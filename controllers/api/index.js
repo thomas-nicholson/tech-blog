b0aaf7a706261a4302102fa3e521a73d17bdc0e3
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-07 21:28:37 +1000

    Added logout route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -28,6 +28,16 @@ router.post('/login', async (req, res) => {
       }
 });
 
+router.post('/logout', (req, res) => {
+  if (req.session.logged_in) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  } else {
+    res.status(404).end();
+  }
+});
+
 router.post('/signup', async (req, res) => {
     try {
         const userData = await User.create(req.body);
