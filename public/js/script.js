45a523c4a3375d8d3b6e3d5467bfd8f5f9bb720c
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-07 23:31:58 +1000

    Added functions to handle posting a new comment, updating a blog post and deleting a blog post

--- public/js/script.js
+++ public/js/script.js
@@ -41,7 +41,6 @@ const signupFormHandler = async (event) => {
     }
 };
   
-
 const newBlogHandler = async (event) => {
   event.preventDefault();
 
@@ -67,7 +66,6 @@ const logOutHandler = async (event) => {
   event.preventDefault();
   const response = await fetch('/api/logout', {
       method: 'POST',
-      //body: JSON.stringify({ title, body }),
       headers: { 'Content-Type': 'application/json' },
   });
 
@@ -76,8 +74,64 @@ const logOutHandler = async (event) => {
   } else {
       alert(response.statusText);
   }
-
 };
+
+const newComment = async (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    const body = document.querySelector('#comment-box').value.trim();
+    const response = await fetch('/api/newcomment', {
+        method: 'POST',
+        body: JSON.stringify({ body, blog_id: event.target.getAttribute("data-blog-id")}),
+        headers: { 'Content-Type': 'application/json' },
+    });
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+  }
+};
+
+const updateBlogHandler = async (event) => {
+  event.preventDefault();
+  const title = document.querySelector('#blog-title').value.trim();
+  const body = document.querySelector('#blog-body').value.trim();
+  const id =   event.target.getAttribute("data-blog-id");
+
+  if (title && body) {
+      const response = await fetch('/api/updatepost', {
+          method: 'PUT',
+          body: JSON.stringify({ title, body, id }),
+          headers: { 'Content-Type': 'application/json' },
+      });
+
+      if (response.ok) {
+          document.location.replace('/dashboard');
+      } else {
+          alert(response.statusText);
+      }
+  }
+};
+
+const deletePost = async (event) => {
+  event.preventDefault();
+  const id = event.target.getAttribute("data-blog-id");
+  var bool = confirm("Are you sure you want to delete this post?");
+  if (bool) {
+    const response = await fetch('/api/deletepost', {
+      method: 'DELETE',
+      body: JSON.stringify({ id }),
+      headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+      document.location.replace('/dashboard');
+  } else {
+      alert(response.statusText);
+  }
+  }
+}
 
 if (document.querySelector('#login-button')) {
   document
@@ -101,4 +155,22 @@ if (document.querySelector('#logout')) {
   document
     .querySelector('#logout')
     .addEventListener('click', logOutHandler);
+}
+
+if (document.querySelector('#comment-box')) {
+  document
+    .querySelector('#comment-box')
+    .addEventListener('keydown', newComment);
+}
+
+if (document.querySelector('#update-blog-button')) {
+  document
+    .querySelector('#update-blog-button')
+    .addEventListener('click', updateBlogHandler);
+}
+
+if (document.querySelector('#delete-button')) {
+  document
+    .querySelector('#delete-button')
+    .addEventListener('click', deletePost);
 }
