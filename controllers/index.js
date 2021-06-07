99c00d43d696632b21c42cca25e5668a26de5320
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-07 23:30:19 +1000

    Updated individual blog post GET route to return comments with authors and conditionally render depending on if the current user is the blog author

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 router.use('/api', apiRoutes);
 
-const { Blog, User } = require('../models');
+const { Blog, User, Comment } = require('../models');
 
 
 router.get('/', async (req, res) => {
@@ -49,10 +49,12 @@ router.get('/newpost', (req, res) => {
         return;
     }
 
-    res.render('newpost');
+    res.render('newpost', {
+        logged_in: req.session.logged_in,
+    });
 });
 
-router.get('/blogs/:id', async (req, res) => {
+router.get('/:id', async (req, res) => {
     if (!req.session.logged_in) {
         res.redirect('/login');
         return;
@@ -61,15 +63,31 @@ router.get('/blogs/:id', async (req, res) => {
         include: [{
             model: User,
             required: true, // do an INNER Join 
+        },
+        {
+            model: Comment,
+            required: false,
+            include: [{
+                model:User,
+                required: false
+            }]
         }],
     });
     console.log(postData);
     const post = postData.get({ plain: true });
     console.log(post);
-    res.render('blog', {
-        post,
-        logged_in: req.session.logged_in,
-    });
+    if (post.user_id === req.session.user_id) {
+        console.log("This is my post!!");
+        res.render('myblog', {
+            post,
+            logged_in: req.session.logged_in,
+        })
+    } else {
+        res.render('blog', {
+            post,
+            logged_in: req.session.logged_in,
+        });
+    }
 });
 
 module.exports = router;
