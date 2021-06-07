8c44d108ecaef62a6355675d82c77f9fd1ee55ad
Thomas Nicholson <24470776+thomas-nicholson@users.noreply.github.com>
2021-06-07 17:32:53 +1000

    Added index get route. Added handlebars middleware and basic handlebars views

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const session = require('express-session');
-// const exphbs = require('express-handlebars');
- const routes = require('./controllers');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
 //Import helpers folder
 //const helpers = require('./utils/helpers');
 
@@ -13,10 +13,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //Links Helpers to handlebars
-//const hbs = exphbs.create({ helpers });
+const hbs = exphbs.create(/*{ helpers }*/);
 
 //Session Middleware
-
 const sess = {
     secret: 'Super secret secret',
     cookie: {},
@@ -29,15 +28,15 @@ const sess = {
 //app.use(session(sess));
 
 //Handlebars+ helpers middleware
-//app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(express.static(path.join(__dirname, 'public')));
 
 //Linking routes folder to express
-//app.use(routes);
+app.use(routes);
 
 //set app to sync to database and listen for requests
 
